e47b62d15e722a7b7a2c211e28b6c2b30059175a
georgiabasa <gbasagianni@ac.upatras.gr>
2025-05-02 15:04:23 -0400

    added actions in p2, 50% bubble sort, and the other 50% between calculation and processing data

--- user/rbac/p2.c
+++ user/rbac/p2.c
@@ -3,6 +3,22 @@
 #include <syscall.h>
 #include <stdio.h>
 
+void sorting_task(int *data, int length) {
+	//simple Bubble Sort for demostration
+	int i = 0;
+	int j = 0;
+	for(i = 0; i < length - 1; i++) {
+		for (j = 0; j < length - i - 1; j++) {
+			if (data[j] > data[j+1]) {
+				int temp = data[j];
+				data[j] = data[j + 1];
+				data[j + 1] = temp;
+			}
+		}
+	}
+	printf("Sorting task completed.\n");
+}
+
 void execute_action(int action_id) {
 	//simulate an action based on the action_id
 	switch(action_id) {
@@ -27,10 +43,17 @@ void execute_action(int action_id) {
 
 int main(int argc, char **argv) {
 	int action_counter = 0;
+	int inside_counter = 0;
+	int data[10] = {9, 7, 5, 3, 2, 8, 1, 4, 6, 10}; //example data for sorting
 
 	while(1) {
 		action_counter++;
-		execute_action(action_counter % 2 + 1); //cycle between action 1 and 2
+		if (action_counter % 2 == 0) {
+			inside_counter++;
+			execute_action(inside_counter % 2 + 1); //regular action like processing data
+		} else {
+			sorting_task(data, 10); //trigger sorting task every other cycle
+		}
 		yield(); //yield to next process
 	}
 }
